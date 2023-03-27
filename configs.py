3bf33e69242c795218d8f9ba1f651742a17de4fd
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2023-03-28 00:38:54 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "5657932594:AAG8GUYld4eqjAkrGee_UFlZ4oSGPl8SMYY")
     FSUB = getenv("FSUB", "JN_MOVIE_REQUIST_GROUP")
     CHID = int(getenv("CHID", "-1001157009177"))
-    SUDO = list(map(int, getenv("SUDO").split(1384893863)))
+    SUDO = list(map(int, getenv("SUDO").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
