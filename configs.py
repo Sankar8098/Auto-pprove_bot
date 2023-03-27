6843244b8410e9b63ffa92a68afb60dcdb14e2ef
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2023-03-28 00:27:57 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "your api id"))
-    API_HASH = getenv("API_HASH", "your api hash")
-    BOT_TOKEN = getenv("BOT_TOKEN", "your bot token")
+    API_ID = int(getenv("API_ID", "15316095"))
+    API_HASH = getenv("API_HASH", "5657932594:AAG8GUYld4eqjAkrGee_UFlZ4oSGPl8SMYY")
+    BOT_TOKEN = getenv("BOT_TOKEN", "5657932594:AAG8GUYld4eqjAkrGee_UFlZ4oSGPl8SMYY")
     FSUB = getenv("FSUB", "your channel username")
     CHID = int(getenv("CHID", "your channel id"))
-    SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb url")
+    SUDO = list(map(int, getenv("SUDO").split(1384893863)))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
