639d6e4db448f77d1864797a6c0937192d267f1c
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2023-03-28 00:34:46 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,8 +4,8 @@ class Config:
     API_ID = int(getenv("API_ID", "15316095"))
     API_HASH = getenv("API_HASH", "5657932594:AAG8GUYld4eqjAkrGee_UFlZ4oSGPl8SMYY")
     BOT_TOKEN = getenv("BOT_TOKEN", "5657932594:AAG8GUYld4eqjAkrGee_UFlZ4oSGPl8SMYY")
-    FSUB = getenv("FSUB", "your channel username")
-    CHID = int(getenv("CHID", "your channel id"))
+    FSUB = getenv("FSUB", "JN_MOVIE_REQUIST_GROUP")
+    CHID = int(getenv("CHID", "-1001157009177"))
     SUDO = list(map(int, getenv("SUDO").split(1384893863)))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority")
     
