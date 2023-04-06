90976e1a2a7221d04b4df426c5069e44d9d1262a
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-04-06 21:49:55 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "15316095"))
-    API_HASH = getenv("API_HASH", "6a293d116082b81260ac83d21f595ffa")
-    BOT_TOKEN = getenv("BOT_TOKEN", "5657932594:AAG8GUYld4eqjAkrGee_UFlZ4oSGPl8SMYY")
-    FSUB = getenv("FSUB", "jn_kannada")
-    CHID = int(getenv("CHID", "-1001848686433"))
-    SUDO = int(getenv("SUDO", "1384893863"))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://jnanesh:jnanesh@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority")
+    API_ID = int(getenv("API_ID", "23990433"))
+    API_HASH = getenv("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20)
+    BOT_TOKEN = getenv("BOT_TOKEN", "5849628330:AAHjBZeXFAM9f2LkgH4OprLoyPycg9VQbhE")
+    FSUB = getenv("FSUB", "SK_MoviesOffl")
+    CHID = int(getenv("CHID", "-1001828551401"))
+    SUDO = int(getenv("SUDO", "5821871362"))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://jnanesh:mongodb+srv://Test:1234@cluster0.2bzsp0q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     
 cfg = Config()
